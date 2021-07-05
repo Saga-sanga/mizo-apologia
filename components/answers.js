1a71b4d67325a7594758dc4314c840b2fb79d32f
Saga-sanga <vulcan248@gmail.com>
2021-07-05 11:09:08 +0530

    repeat

--- components/answers.js
+++ components/answers.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import AnswerCard from './AnswerCard.js'
+import AnswerCard from './answerCard.js'
 import styles from '../styles/Home.module.css'
 
 const Answers = ({ answers }) => {
