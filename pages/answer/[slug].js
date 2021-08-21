6ba68e87c3569d8d2949770742116a76f7c20b19
Saga-sanga <vulcan248@gmail.com>
2021-08-21 21:31:02 +0530

    added backlins to article and answer slugs

--- pages/answer/[slug].js
+++ pages/answer/[slug].js
@@ -5,6 +5,7 @@ import { fetchAPI } from "../../lib/api";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 import { getStrapiMedia } from "../../lib/media";
+import Link from "next/link";
 
 const Answer = ({ answer }) => {
   const imageUrl = getStrapiMedia(answer.image);
@@ -28,8 +29,18 @@ const Answer = ({ answer }) => {
       >
         <h1 className='contentTitleBlur'>{answer.title}</h1>
       </div>
-      <div className="uk-section">
+      <div className="uk-section" style={{paddingTop: '25px'}}>
         <div className="uk-container textAreaContainer">
+          <div>
+            <Link href="/answers">
+              <a className="articleLink">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-left">
+                  <polyline points="15 18 9 12 15 6"></polyline>
+                </svg>
+                Chhannate
+              </a>
+            </Link>
+          </div>
           <ReactMarkdown rehypePlugins={[rehypeRaw]} children={answer.content}/>
           <hr className="uk-divider-small" />
           <div className="uk-grid-small uk-flex-left" data-uk-grid="true">
--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -6,6 +6,7 @@ import Layout from "../../components/layout";
 import Image from "../../components/image";
 import Seo from "../../components/seo";
 import { getStrapiMedia } from "../../lib/media";
+import Link from "next/link";
 
 const Article = ({ article }) => {
   const imageUrl = getStrapiMedia(article.image);
@@ -29,8 +30,18 @@ const Article = ({ article }) => {
       >
         <h1 className='contentTitleBlur'>{article.title}</h1>
       </div>
-      <div className="uk-section">
+      <div className="uk-section" style={{paddingTop: '25px'}}>
         <div className="uk-container textAreaContainer">
+          <div>
+            <Link href="/answers">
+              <a className="articleLink">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-chevron-left">
+                  <polyline points="15 18 9 12 15 6"></polyline>
+                </svg>
+                Thuziakte
+              </a>
+            </Link>
+          </div>
           <ReactMarkdown rehypePlugins={[rehypeRaw]} children={article.content}/>
           {
             (article.endNote === null || article.endNote == '') ? '' 
