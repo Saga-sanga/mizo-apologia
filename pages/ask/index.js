9856eeeac0e6a205482b0aad053ca8f1201d1c64
Saga-sanga <vulcan248@gmail.com>
2021-09-15 22:04:18 +0530

    added instruction section to ask

--- pages/ask/index.js
+++ pages/ask/index.js
@@ -34,6 +34,7 @@ function Ask() {
         <div className={`topic-body ${styles.formBody}`}>
           <h1 className={styles.headingMargin}>Zawhna Zawt Rawh</h1>
           <h5 className={styles.headingMargin}>A hnuaia form hmang hian i zawhna zawt rawh le</h5>
+          
           <form onSubmit={handleSubmit} className={styles.contactForm}>
             <div className={styles.rowField}>
               <div className={styles.formField}>
@@ -71,6 +72,17 @@ function Ask() {
             />
             <button className={styles.submitButton} type="submit">Submit</button>
           </form>
+          <div>
+            <h3 style={{margin: '3rem 0 0.4rem 0'}}>Zawhna Thehluhna Chhungchanga Kaihhruaina</h3>
+            <p style={{margin: 0}}>Zawhna I thehluh hunah, heng a hnuaia kaihhruainate hi zawm ang che.
+              A hnuaia mite hi zawm a nih loh chuan, i zawhna thehluhte chu chhan an ni lo vang.</p>
+            <ol>
+              <li>Tawngkam mawi lo, hmuhsitna tawngkam te hmang suh ang che (Efesi 4:29).</li>
+              <li>A zawhna chu mawi hnai takin zawt ang che (1 Peter 3:15).</li>
+              <li>Inhnialna rilru puin zawt lo hram ang che, inhnialna rawngbawlna kan nei lova,
+                thinlung taka thutak zawngtute tanpui kan duh a ni (1 Timothea 6:20).</li>
+            </ol>
+          </div>
         </div>
       </main>
     </Layout>
